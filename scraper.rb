12da8f50e977c7cea394296d66d04217e65d9488
Tony Bowden <tony@tmtm.com>
2015-09-12 20:00:46 +0100

    toggle between en & uz

--- scraper.rb
+++ scraper.rb
@@ -19,42 +19,52 @@ def noko_for(url)
   Nokogiri::HTML(open(url).read)
 end
 
-def scrape_letters(url)
-  noko = noko_for(url)
+def scrape_letters(fmt, lang)
+  url = fmt % lang
+  noko = noko_for(url % lang)
   noko.css('#alfabetDepsSearch li a/@href').each do |href|
     link = URI.join url, href
-    scrape_list(link)
+    scrape_list(link, lang)
   end
 end
 
-def scrape_list(url)
+def scrape_list(url, lang)
   puts url.to_s.cyan
   noko = noko_for(url)
   seen = Set.new
   noko.css('.deputiesList li a[href*="/deputy/"]/@href').each do |href|
     link = URI.join url, href
     next if seen.include? link
-    scrape_person(link, { 
+    scrape_person(link, lang, { 
       image: href.parent.css('img/@src').text,
     })
     seen << link
   end
 end
 
-def scrape_person(url, data_i)
+def scrape_person(url, lang, data_i)
   noko = noko_for(url)
 
-  add_map = { 
+  add_map = lang == 'uz' ? { 
     region: "Худуд",
     constituency: "Сайлов округи",
     party: "Партияга мансублиги",
     faction: "Фракцияга аъзолиги",
+  } : { 
+    region: "Region",
+    constituency: "Constituency",
+    party: "Party",
+    faction: "Faction member",
   }
 
-  info_map = { 
+  info_map = lang == 'uz' ? { 
     birth_date: "Туғилган сана",
     birth_place: "Туғилган жойи",
     nationality: "Миллати",
+  } : {
+    birth_date: "Date of Birth",
+    birth_place: "Place of Birth",
+    nationality: "Nationality",
   }
 
   info = noko.css('#deputyPersInfo')
@@ -76,9 +86,10 @@ def scrape_person(url, data_i)
   }.merge(data_i).merge(info_data).merge(add_data)
   data[:image] = URI.join(url, URI.encode(data[:image])).to_s unless data[:image].to_s.empty?
   data[:birth_date] = data[:birth_date].split('.').reverse.join('-')
-  puts data
+  puts data[:id]
   # ScraperWiki.save_sqlite([:id, :term], data)
 end
 
-scrape_letters('http://www.parliament.gov.uz/uz/structure/deputy/')
+@LANG = ARGV.first || 'uz'
+scrape_letters('http://www.parliament.gov.uz/%s/structure/deputy/', @LANG)
 # scrape_letters('http://www.parliament.gov.uz/en/structure/deputy/search1.php?id=+')
