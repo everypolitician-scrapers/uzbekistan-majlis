c5903c74d4f2bfbda61b116929ee332cca69ea9e
Tony Bowden <tony@tmtm.com>
2015-09-12 18:49:08 +0100

    Get info from correct box

--- scraper.rb
+++ scraper.rb
@@ -50,10 +50,10 @@ def scrape_person(url, data_i)
     birth_place: info.xpath('.//td[text()="Place of Birth"]/following-sibling::td').text,
     nationality: info.xpath('.//td[text()="Nationality"]/following-sibling::td').text,
 
-    region: info.xpath('.//li[contains(.,"Region:")]').text.split(':',2).last.to_s.tidy,
-    constituency: info.xpath('.//li[contains(.,"Constituency:")]').text.split(':',2).last.to_s.tidy,
-    party: info.xpath('.//li[contains(.,"Party:")]').text.split(':',2).last.to_s.tidy,
-    faction: info.xpath('.//li[contains(.,"Faction member:")]').text.split(':',2).last.to_s.tidy,
+    region: add.xpath('.//li[contains(.,"Region:")]').text.split(':',2).last.to_s.tidy,
+    constituency: add.xpath('.//li[contains(.,"Constituency:")]').text.split(':',2).last.to_s.tidy,
+    party: add.xpath('.//li[contains(.,"Party:")]').text.split(':',2).last.to_s.tidy,
+    faction: add.xpath('.//li[contains(.,"Faction member:")]').text.split(':',2).last.to_s.tidy,
     term: 5,
     source: url.to_s,
   }.merge(data_i)
